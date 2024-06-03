4eb8f57baf9d12139a46c6c35d237ad5b4c199c4
szmbzra <szmbzra@gmail.com>
2024-06-03 22:54:31 +0545

    slide updated

--- src/Layout/Home.jsx
+++ src/Layout/Home.jsx
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
-import { rentBuy, slider } from '../components/data';
-import Title from '../components/Title';
+import React, { useState, useRef } from "react";
+import { rentBuy, slider } from "../components/data";
+import Title from "../components/Title";
 import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
 
 const Home = () => {
   const [background, setBackground] = useState(true);
-  console.log(background);
+
+  const wrapperRef = useRef(null);
+  const scrollAmount = 500;
 
   const ToggleRent = () => {
     setBackground(true);
@@ -14,7 +16,20 @@ const Home = () => {
   const ToggleBuy = () => {
     setBackground(false);
   };
-   return (
+
+  const Next = () => {
+    if (wrapperRef.current) {
+      wrapperRef.current.scrollLeft += scrollAmount;
+    }
+  };
+
+  const Prev = () => {
+    if (wrapperRef.current) {
+      wrapperRef.current.scrollLeft -= scrollAmount;
+    }
+  };
+
+  return (
     <div>
       {/* slider banner */}
       <div className="bg-slate-500 h-[600px] flex">
@@ -27,7 +42,8 @@ const Home = () => {
                   for future life
                 </h1>
                 <p className="pe-10">
-                  Find a variety of properties that suit you very easily, forget all difficulties in finding a residence for you
+                  Find a variety of properties that suit you very easily, forget
+                  all difficulties in finding a residence for you
                 </p>
               </div>
             </div>
@@ -35,7 +51,9 @@ const Home = () => {
               <div className="flex">
                 <button
                   className={`w-28 h-12 rounded-tl-lg font-bold ${
-                    background ? 'bg-defaultColor text-white' : 'bg-orange-100 text-black'
+                    background
+                      ? "bg-defaultColor text-white"
+                      : "bg-orange-100 text-black"
                   }`}
                   onClick={ToggleRent}
                 >
@@ -43,7 +61,9 @@ const Home = () => {
                 </button>
                 <button
                   className={`w-28 h-12 rounded-tr-lg font-bold ${
-                    background ? 'bg-orange-100 text-black' : 'bg-defaultColor text-white'
+                    background
+                      ? "bg-orange-100 text-black"
+                      : "bg-defaultColor text-white"
                   }`}
                   onClick={ToggleBuy}
                 >
@@ -51,32 +71,38 @@ const Home = () => {
                 </button>
               </div>
               <div className="bg-white w-[441px] h-auto p-6 rounded-tr-md rounded-bl-md rounded-br-md">
-     { background ? (
-      <>
-                <div className="h-12 w-full">
-                  <input
-                    type="text"
-                    placeholder="Type keyword..."
-                    className="placeholder:text-slate-400 h-14 rounded-md w-full border-2 border-neutral-200 ps-5"
-                  />
-                </div>
-                <div className="w-full mt-6">
-                  <select className="h-12 w-full border-2 border-neutral-200 ps-5">
-                    <option value="">Property Type</option>
-                  </select>
-                </div>
-                <div className="h-12 w-full mt-6">
-                  <select className="h-12 w-full border-2 border-neutral-200 ps-5">
-                    <option value="">Location</option>
-                  </select>
-                </div>
-      </>
-     ): (<div className="text-center text-xl text-gray-700">
-     Coming Soon
-   </div>)}
+                {background ? (
+                  <>
+                    <div className="h-12 w-full">
+                      <input
+                        type="text"
+                        placeholder="Type keyword..."
+                        className="placeholder:text-slate-400 h-14 rounded-md w-full border-2 border-neutral-200 ps-5"
+                      />
+                    </div>
+                    <div className="w-full mt-6">
+                      <select className="h-12 w-full border-2 border-neutral-200 ps-5">
+                        <option value="">Property Type</option>
+                      </select>
+                    </div>
+                    <div className="h-12 w-full mt-6">
+                      <select className="h-12 w-full border-2 border-neutral-200 ps-5">
+                        <option value="">Location</option>
+                      </select>
+                    </div>
+                  </>
+                ) : (
+                  <div className="text-center text-xl text-gray-700">
+                    Coming Soon
+                  </div>
+                )}
                 <div className="flex justify-between gap-2 mt-6">
-                  <button className="bg-orange-300 grow h-12 rounded-md hover:bg-defaultColor ">Filter</button>
-                  <button className="bg-orange-300 grow h-12 rounded-md hover:bg-defaultColor">Search Now</button>
+                  <button className="bg-orange-300 grow h-12 rounded-md hover:bg-defaultColor ">
+                    Filter
+                  </button>
+                  <button className="bg-orange-300 grow h-12 rounded-md hover:bg-defaultColor">
+                    Search Now
+                  </button>
                 </div>
               </div>
               <ul className="flex justify-between mt-3 w-[441px] text-white">
@@ -99,28 +125,36 @@ const Home = () => {
       </div>
       {/* Slider */}
       <div className="container mx-auto">
- <div className="image_slider">
-<div className="image_wrapper  fle">
-<div className="flex justify-between text-center">
-          {slider.map(({ id, name, listing }) => (
-            <div className="items" key={id}>
-              <div className="rounded-full h-28 w-28 bg-slate-300 mb-3"></div>
-              <h2 className="font-bold">{name}</h2>
-              <p>{listing} listing</p>
+        <div className="image_slider relative">
+          <div
+            ref={wrapperRef}
+            className="image_wrapper overflow-x-auto w-full whitespace-nowrap"
+          >
+            <div className="flex justify-evenly text-center w-[2600px]">
+              {slider.map(({ id, name, listing }) => (
+                <div className="items inline-block" key={id}>
+                  <div className="rounded-full h-28 w-28 bg-slate-300 mb-3"></div>
+                  <h2 className="font-bold">{name}</h2>
+                  <p>{listing} listing</p>
+                </div>
+              ))}
             </div>
-          ))}
+          </div>
+          <div className="flex justify-between absolute w-full top-1/2 transform -translate-y-1/2">
+            <div className="hover:cursor-pointer">
+              <button onClick={Prev}>
+                <FaArrowLeft />
+              </button>
+            </div>
+            <div className="hover:cursor-pointer">
+              <button onClick={Next}>
+                <FaArrowRight />
+              </button>
+            </div>
+          </div>
         </div>
-</div>
-        <div className="flex  justify-between">
-<div className="hover:cursor-pointer">
-</div>
-<div className="hover:cursor-pointer ">
-<button><FaArrowRight /></button>
-
-</div>
-        </div>
- </div>
       </div>
+      {/* slider */}
     </div>
   );
 };
--- src/components/data.js
+++ src/components/data.js
@@ -1,22 +1,28 @@
 export const navbar = [
-    {id:1,  name:"Home", urlLink: "/Home"},
-    {id:2,  name:"Property", urlLink: "/Property"},
-    {id:3,  name:"Page", urlLink: "/Page"},
-    {id:4,  name:"Blog", urlLink: "/Blog"},  
-    {id:5,  name:"Contact", urlLink: "/Contact"},  
+  { id: 1, name: "Home", urlLink: "/Home" },
+  { id: 2, name: "Property", urlLink: "/Property" },
+  { id: 3, name: "Page", urlLink: "/Page" },
+  { id: 4, name: "Blog", urlLink: "/Blog" },
+  { id: 5, name: "Contact", urlLink: "/Contact" },
 ];
 export const rentBuy = [
-    { id:1, name: "Houses"},
-    { id:2, name: "Villa"},
-    { id:3, name: "Office"},
-    { id:4, name: "Apartments"}
+  { id: 1, name: "Houses" },
+  { id: 2, name: "Villa" },
+  { id: 3, name: "Office" },
+  { id: 4, name: "Apartments" },
 ];
 
 export const slider = [
-    { id:1, name: "Chile", listing: 1570},
-    { id:2, name: "Banglore", listing:1570},
-    { id:3, name: "New Delhi", listing:1570},
-    { id:4, name: "Berlin", listing:1570},
-    { id:5, name: "California", listing:1570},
-    { id:6, name: "Brazil", listing:1570}
+  { id: 1, name: "Chile", listing: 1570 },
+  { id: 2, name: "Banglore", listing: 1570 },
+  { id: 3, name: "New Delhi", listing: 1570 },
+  { id: 4, name: "Berlin", listing: 1570 },
+  { id: 5, name: "California", listing: 1570 },
+  { id: 6, name: "Brazil", listing: 1570 },
+  { id: 7, name: "Chile", listing: 1570 },
+  { id: 8, name: "Banglore", listing: 1570 },
+  { id: 9, name: "New Delhi", listing: 1570 },
+  { id: 10, name: "Berlin", listing: 1570 },
+  { id: 11, name: "California", listing: 1570 },
+  { id: 12, name: "Brazil", listing: 1570 },
 ];
